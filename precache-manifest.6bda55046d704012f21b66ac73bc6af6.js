813556b8bed701a95643b4e8306f3c9f31092938
YuYu <jasonko12033@gmail.com>
2020-01-09 23:39:59 -0500

    Updates

---
+++ precache-manifest.6bda55046d704012f21b66ac73bc6af6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff76be2175fdf665b66429c29a8f1201",
+    "url": "/RobotFriends/index.html"
+  },
+  {
+    "revision": "c8890da5dfac99701af1",
+    "url": "/RobotFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f7516fa7ede5ffb11efd",
+    "url": "/RobotFriends/static/css/main.7e6016d9.chunk.css"
+  },
+  {
+    "revision": "c8890da5dfac99701af1",
+    "url": "/RobotFriends/static/js/2.a98415d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RobotFriends/static/js/2.a98415d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7516fa7ede5ffb11efd",
+    "url": "/RobotFriends/static/js/main.55c4db35.chunk.js"
+  },
+  {
+    "revision": "51033cbd019d88ce8bd4",
+    "url": "/RobotFriends/static/js/runtime-main.864bfd7d.js"
+  },
+  {
+    "revision": "2e5ec5b3e538df515284bcc246b0629d",
+    "url": "/RobotFriends/static/media/Strawberry Shortcake Supplement Logo.2e5ec5b3.woff"
+  }
+]);
